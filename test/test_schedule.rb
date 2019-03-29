b3be98f5a80fa522f4e8b119f98d06cb09c05719
Steffen Müller <git@xonx.de>
2019-03-29 11:18:26 +0100

    Add spec for nested config

--- test/test_schedule.rb
+++ test/test_schedule.rb
@@ -12,6 +12,18 @@ class TestSchedule < Sidecloq::Test
         }
       }
     end
+    let(:nested_schedule_hash) do
+      {
+        'test' => {
+          'test_job' => {
+            'class' => 'JobClass',
+            'cron' => '0 7 * * *',
+            'queue' => 'default',
+            'args' => { 'batch' => 100 }
+          }
+        }
+      }
+    end
     let(:schedule) { Sidecloq::Schedule.from_hash(schedule_hash) }
     before { Sidekiq.redis(&:flushdb) }
 
@@ -21,6 +33,23 @@ class TestSchedule < Sidecloq::Test
       file = Tempfile.new('schedule_test')
 
       schedule.save_yaml(file.path)
+
+      loaded = Sidecloq::Schedule.from_yaml(file.path)
+
+      assert_equal('test_job', loaded.job_specs.keys.first)
+      assert_equal('0 7 * * *', loaded.job_specs.values.first['cron'])
+      assert_equal({'batch' => 100}, loaded.job_specs.values.first['args'])
+
+      file.delete
+    end
+
+    it 'can load by env from a nested yml file' do
+      require 'tempfile'
+
+      file = Tempfile.new('nested_schedule_test')
+
+      rails = class_double('Rails')
+      expect(rails).to receive(:env).and_return 'test'
 
       loaded = Sidecloq::Schedule.from_yaml(file.path)
 
